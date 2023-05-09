f1209e3bffde64e0b1e4aae2af64e19cef4a8397
Phuong Anh <nhuphuonganh001@..com>
2023-05-09 10:28:00 +0200

    first test

---
+++ cypress/intergration/examples/Test1.js
@@ -0,0 +1,7 @@
+// Cypress - spec
+
+describe('My first test suite', function() {
+    it('My first test case', function() {
+    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+    })
+})
