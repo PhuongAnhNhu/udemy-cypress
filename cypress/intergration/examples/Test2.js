9288c1e3981c300c53fb827dcbe51a93ede363ef
Ext.Anh-Nhu.Phuong <phuong.anh-nhu.extdguv.de>
2023-05-20 16:31:40 +0200

    test

--- cypress/intergration/examples/Test2.js
+++ cypress/intergration/examples/Test2.js
@@ -5,6 +5,7 @@ describe('My first test suite', function() {
     cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
     cy.xpath('//input[@class="search-keyword"]').type('ca');
     cy.wait(2000);
-    cy.xpath('//div[@class="product"]').should('have.length', 4)
+    cy.xpath('//div[@class="product"]').should('have.length', 4);
+    cy.log("TEST");
     })
 })
