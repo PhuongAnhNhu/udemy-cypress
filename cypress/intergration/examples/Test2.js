f481b3bcdbed85fc3d9aab5bf1a55cae8749b453
Phuong Anh <nhuphuonganh001@..com>
2023-06-06 12:57:27 +0200

    update

--- cypress/intergration/examples/Test2.js
+++ cypress/intergration/examples/Test2.js
@@ -1,10 +1,30 @@
 /// <reference types="Cypress" />
 
-describe('My first test suite', function() {
-    it('My first test case', function() {
-    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
-    cy.xpath('//input[@class="search-keyword"]').type('ca');
-    cy.wait(2000);
-    cy.xpath('//div[@class="product"]').should('have.length', 4)
-    })
-})
+describe("My first test suite", function () {
+    it("My first test case", function () {
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+        cy.xpath('//input[@class="search-keyword"]').type("ca");
+        cy.wait(2000);
+        // cy.get('.product:visible').should('have.length', 4)
+        cy.xpath('//div[@class="product"]').should("have.length", 4);
+
+        // Parent child chaining
+        // cy.get('.products').find('.product').should('have.length', 4);
+        cy.xpath('//div[@class="products"]/div[@class="product"]').should("have.length", 4);
+
+        /** get element by index nummer */
+        // cy.get('.products').find('.product').eq(1).contains('ADD TO CART').click();
+        cy.xpath('//div[@class="product"][2]//button').click();
+
+        /** get element by name */
+        cy.xpath('//div[@class="product"]')
+            .should("have.length", 4)
+            .each(($element, index, list) => {
+                const productName = $element.find(".product-name").text();
+                if (productName.includes("Cashews")) {
+                    // need to use wrap cause $element is a promise
+                    cy.wrap($element).find(`button`).click();
+                }
+            });
+    });
+});
